4fe9d6d89a532af8ee39769a809e47ab14a35ad7
Ryan <rjmpoon@gmail.com>
2022-12-15 16:37:21 -0500

    get motor running

--- src/motor_tests/motor_tests/grasp_motor_test.py
+++ src/motor_tests/motor_tests/grasp_motor_test.py
@@ -3,20 +3,40 @@ from rclpy.node import Node
 from std_msgs.msg import String
 
 from tinkerforge.ip_connection import IPConnection
-from tinkerforge.brick_dc import BrickDC
+from tinkerforge.bricklet_servo_v2 import BrickletServoV2
+from tinkerforge.bricklet_io4_v2 import BrickletIO4V2
 
 HOST = "localhost"
 PORT = 4223
-UID_dc = "6RPLkK"
+UID_servo = "SNw"
+UID_io2 = "QAuAz"
+UID_master = "646H2t"
 
 if __name__ == '__main__':
-    msg = "hello everyone"
+    msg = "hello people"
     print(msg)
 
     ipcon = IPConnection()
-    dc = BrickDC(UID_dc, ipcon)
+    io = BrickletIO4V2(UID_io2, ipcon)
+    servo = BrickletServoV2(UID_servo, ipcon)
     ipcon.connect(HOST, PORT)
 
-    print(dc.get_current_consumption())
+    # Set settings 100, 50, 34, 
+    servo.set_period(0, 200)
+    servo.set_degree(0, 0, 6000)
+    print(servo.get_degree(0))
+    print(servo.get_period(0))
+
+    # Send PWM signal
+    input("Press Enter to start")
+    servo.set_position(0, 6000)
+    servo.set_enable(0, True)
+    print("Should have started")
+
+    # stop the motion
+    input("Press Enter to stop")
+    servo.set_position(0, 0)
+    servo.set_enable(0, False)
+    print("Should have stopped")
 
     ipcon.disconnect()
