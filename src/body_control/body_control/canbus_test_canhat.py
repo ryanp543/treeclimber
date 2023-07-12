023c7be93551b94841e1ae1da99e1c25bd91dc9d
Ryan <rjmpoon@gmail.com>
2023-07-12 11:15:39 -0400

    can for forum

--- src/body_control/body_control/canbus_test_canhat.py
+++ src/body_control/body_control/canbus_test_canhat.py
@@ -1,3 +1,4 @@
+# sudo nano /boot/firmware/config.txt
 # pip install python-can
 # if error RTNETLINK answers: Device or resource busy
 # command in terminal: sudo /sbin/ip link set can0 down
@@ -6,12 +7,11 @@ import can
 import time
 
 os.system('sudo /sbin/ip link set can0 down')
-os.system('sudo ip link set can0 type can bitrate 1000000')
+os.system('sudo ip link set can0 type can bitrate 500000')
 os.system('sudo ifconfig can0 up')
 
-with can.interface.Bus(channel='can0', interface='socketcan', bitrate=1000000) as bus:
-    print("yeah ya")
-    msg = can.Message(arbitration_id=0x1, data=[1,3,5], is_extended_id=True)
+with can.interface.Bus(channel='can0', interface='socketcan', bitrate=500000) as bus:
+    msg = can.Message(arbitration_id=0x123, data=[1,3,5], is_extended_id=False)
     bus.send(msg)
 
 print('hello world')
