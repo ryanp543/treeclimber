af179a0f9c62fa4d0ed9890c0d889fa08ee3f4bb
Ryan <rjmpoon@gmail.com>
2023-07-19 17:44:06 -0400

    created functions to convert list of values into bytes to be sent

--- src/body_control/body_control/canbus_test_canhat.py
+++ src/body_control/body_control/canbus_test_canhat.py
@@ -6,6 +6,21 @@ import os
 import can
 import time
 
+POS_MAX = 31.0         # 10pi =~ 31 rad (around 5 whole rotations)
+POS_MIN = -31.0
+VEL_MAX = 4.0          # 41 rpm =~ 4 rad/s for 70 kg-cm torque
+VEL_MIN = -4.0
+SERVO_MAX = 270.0
+SERVO_MIN = 0.0
+KP_MAX = 100.0
+KP_MIN = 0.0
+KD_MAX = 10.0
+KD_MIN = 0.0
+KI_MAX = 30.0
+KI_MIN = 0.0
+
+# Function receive_status
+# Callback function for incoming CANBus messages from the main body
 def receive_status(msg):
     id = msg.arbitration_id
     data = msg.data
@@ -15,11 +30,65 @@ def receive_status(msg):
         value_new = (data[k] << 8) + data[k+1]
         values_received.append(value_new)
 
-    print(id)
-    print(values_received)
+    if id == 0:
+        print(id)
+        print(values_received)
+
+
+# Function map_to_uint16
+# Takes a value, a maximum, and a minimum and outputs a 16 bit integer that
+# represents the value between the bounds. Note that any value above the max
+# or below the min will be set to the max or the min, respectively. 
+def map_to_uint16(value, max_value, min_value):
+    value_range = max_value - min_value
+    value = max(min_value, min(value, max_value))
+    scaled_value = int(((value - min_value) / value_range) * 65535)
+
+    return scaled_value & 0xFFFF
+
+
+# Function create_command
+# Takes the message id (which dictates the type of command) and the corresponding
+# list of values and returns the 8 bytes of data that will be sent to the Teensy 
+# on the main body. 
+def create_command(id, value_list):
+    # Depending on id, creates a list of the relevant maximums and minimums
+    # See comments in send_command function
+    match id:
+        # ID 0x1: Position
+        case 0x1:
+            maximums = [POS_MAX, KP_MAX, KD_MAX, KI_MAX]
+            minimums = [POS_MIN, KP_MIN, KD_MIN, KI_MIN]
+        # ID 0x2: Velocity
+        case 0x2:
+            maximums = [VEL_MAX, KP_MAX, KD_MAX, KI_MAX]
+            minimums = [VEL_MIN, KP_MIN, KD_MIN, KI_MIN]
+        # ID 0x3: Main servo roll
+        case 0x3:
+            maximums = [SERVO_MAX, 1.0, 1.0, 1.0]
+            minimums = [SERVO_MIN, 0.0, 0.0, 0.0]
+        case _:
+            print("DESIRED COMMAND ID NOT WITHIN RANGE.")
+    
+    # Creates a scaled value from each value in the value list
+    # Converts it to bytes and adds it to the command byte array
+    # scaled_value_list = []
+    cmd = bytearray()
+    for k in range(len(value_list)):
+        scaled_value = map_to_uint16(value_list[k], maximums[k], minimums[k])
+        # scaled_value_list.append(scaled_value)
+        cmd.extend(scaled_value.to_bytes(2, 'big')) 
+    
+    # print(scaled_value_list)
+    print(cmd)
+
+    # Returns the 8 bytes of data to be sent representing the uint_16 values
+    return cmd
+
+
+# Function send_commands
 
 def send_commands(bus):
-
     # Message IDs for different commands
     # 0x1 = wheel position (value, P, I, D)
     # 0x2 = wheel velocity
@@ -31,24 +100,16 @@ def send_commands(bus):
     # 0x8 = clippers
     msg_id = 0x1
 
-    # How to encode convert strings to bytes
-    feature = "p"
-    feature_bytes = feature.encode()
-
+    # Take command and maps to unsigned 16 bit integer values
     # Message packet is 8 bytes, typically four 16 bit unsigned integers
-    values = [65535, 31231, 1511, 226] # 16 bit integers unsigned
-    command = bytearray()
-    for k in range (len(values)):
-        command.extend(values[k].to_bytes(2, 'big')) 
-
-    print(command)
-    print(len(command))
+    command = create_command(msg_id, [-24.6, 40.05, 7.2, 0.4])
 
     # Open can bus interface and send the command
     msg = can.Message(arbitration_id=msg_id, data=command, is_extended_id=False)
     bus.send(msg)
 
     print("Command sent!")
+
 
 if __name__ == "__main__":
     # Run OS terminal commands to set up and open CANBus ports
