32c3a7867fd172f7166fab76f9581d4a22ecd6bc
Ryan <rjmpoon@gmail.com>
2024-07-09 19:27:43 -0400

    right arm tighten

--- src/body_control/body_control/arms_tighten.py
+++ src/body_control/body_control/arms_tighten.py
@@ -6,7 +6,7 @@ import time
 # 0x1 = wheel position (value, P, I, D)
 # 0x2 = wheel velocity
 # 0x3 = turret motor position
-# 0x4 = left tendon
+# 0x4 = left tendon [torque setpoint, pos setpoint or time(s), kp, ki]
 # 0x5 = right tendon
 # 0x6 = (facing from back tail counterweight) left = 2, right = 3 servo roll
 # 0x7 = clippers
@@ -14,9 +14,19 @@ import time
 # 0xb = get drive encoder data
 
 def main():
+    # Create my_controller object
     my_controller = body_controller.MyController()
-    my_controller.send_command(1, [2*math.pi, 40, 1, 0.1])
+
+    # Wind up right arm tendon using position command based on matlab Kinematics.m 
+    # Finish with torque 2.9850
+    # my_controller.send_command(5, [0, 2.9850, 0, 0])
+    # my_controller.send_command(5, [2, 3, 1, 2.2])
     time.sleep(1)
+    # my_controller.send_command(5, [0, -3.5, 0, 0])
+
+    # my_controller.send_command(3, [-math.pi/2, 25, 2, 5])
+
+    # Shutdown controller object
     my_controller.shutdown()
 
 if __name__ == "__main__":
